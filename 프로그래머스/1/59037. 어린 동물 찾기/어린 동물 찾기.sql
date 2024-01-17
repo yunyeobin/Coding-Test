ac714ccf76cf89266c97bb5df6f520d0719a5a8c
Yun Yeobin <152828518+yunyeobin@users.noreply.github.com>
2024-01-17 21:09:48 +0900

    [level 1] Title: 어린 동물 찾기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/1/59037. 어린 동물 찾기/어린 동물 찾기.sql
+++ 프로그래머스/1/59037. 어린 동물 찾기/어린 동물 찾기.sql
@@ -1,3 +1,3 @@
 -- 코드를 입력하세요
 
-SELECT animal_id, name from animal_ins where intake_condition not like 'aged' order by animal_id;
+SELECT animal_id, name from animal_ins where intake_condition not like 'aged';
