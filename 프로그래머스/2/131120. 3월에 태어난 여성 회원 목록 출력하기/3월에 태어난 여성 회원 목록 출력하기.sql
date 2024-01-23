66aceb968b2cbc86a151ce51be7a1ad7a9bd8761
Yun Yeobin <152828518+yunyeobin@users.noreply.github.com>
2024-01-23 15:06:32 +0900

    [level 2] Title: 3월에 태어난 여성 회원 목록 출력하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

---
+++ 프로그래머스/2/131120. 3월에 태어난 여성 회원 목록 출력하기/3월에 태어난 여성 회원 목록 출력하기.sql
@@ -0,0 +1,2 @@
+-- 코드를 입력하세요
+SELECT member_id, member_name, gender, date_format(date_of_birth, '%Y-%m-%d') from member_profile where gender = 'W' and date_format(date_of_birth, '%m') = '03' and tlno is not null order by member_id;
