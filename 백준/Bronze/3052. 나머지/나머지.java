b5d688c22d92bf39fa2b5b47a8471ccd23ffeccb
Yun Yeobin <152828518+yunyeobin@users.noreply.github.com>
2024-06-30 23:27:06 +0900

    [Bronze II] Title: 나머지, Time: 176 ms, Memory: 17648 KB -BaekjoonHub

---
+++ 백준/Bronze/3052. 나머지/나머지.java
@@ -0,0 +1,21 @@
+import java.util.HashSet;
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+	
+		Scanner sc = new Scanner(System.in);
+		HashSet<Integer> h = new HashSet<Integer>();
+		
+		for(int i = 0; i<10; i++) {
+			h.add(sc.nextInt() % 42);
+		}
+		
+		sc.close();
+		System.out.println(h.size());
+		
+		
+	}
+
+}
