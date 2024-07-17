0fe8e95dd55e8260a3c887647dad42aeef3d42f2
Yun Yeobin <152828518+yunyeobin@users.noreply.github.com>
2024-07-18 06:01:28 +0900

    [Bronze V] Title: 1998년생인 내가 태국에서는 2541년생?!, Time: 208 ms, Memory: 17736 KB -BaekjoonHub

---
+++ 백준/Bronze/18108. 1998년생인 내가 태국에서는 2541년생？！/1998년생인 내가 태국에서는 2541년생？！.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int y = sc.nextInt();
+        int year = y-543;
+        System.out.println(year);
+        sc.close();
+    }
+}
