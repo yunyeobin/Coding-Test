70b61e9d43738ef503b1f354c200b15f6cfcb7b1
Yun Yeobin <152828518+yunyeobin@users.noreply.github.com>
2024-11-18 18:16:09 +0900

    [Bronze V] Title: We love kriii, Time: 92 ms, Memory: 13992 KB -BaekjoonHub

---
+++ 백준/Bronze/10718. We love kriii/We love kriii.java
@@ -0,0 +1,21 @@
+
+import java.io.*;
+import java.util.Arrays;
+import java.util.Scanner;
+import java.util.Stack;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException{
+//        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+//        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+//        Scanner sc = new Scanner(System.in);
+        String str = "강한친구 대한육군";
+        System.out.println(str);
+        System.out.println(str);
+//        bw.flush();
+//        bw.close();
+//        br.close();
+
+    }
+}
