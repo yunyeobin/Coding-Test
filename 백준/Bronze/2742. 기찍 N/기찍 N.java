67fe750c866c5ec647ddf785f3f2ca3b597359ca
Yun Yeobin <152828518+yunyeobin@users.noreply.github.com>
2024-11-18 18:19:30 +0900

    [Bronze IV] Title: 기찍 N, Time: 796 ms, Memory: 34052 KB -BaekjoonHub

---
+++ 백준/Bronze/2742. 기찍 N/기찍 N.java
@@ -0,0 +1,21 @@
+
+import java.io.*;
+import java.util.Arrays;
+import java.util.Scanner;
+import java.util.Stack;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException{
+//        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+//        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        Scanner sc = new Scanner(System.in);
+        int N = sc.nextInt();
+        for (int i = N; i > 0; i--) {
+            System.out.println(i);
+        }
+
+
+
+    }
+}
